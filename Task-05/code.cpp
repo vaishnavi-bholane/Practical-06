8108265f86fbd6020b224214326f8ff267c7a89d
Vaishnavi Bholane <87429198+vaishnavi-bholane@users.noreply.github.com>
2021-10-12 15:34:00 +0530

    Add files via upload

---
+++ Task-05/code.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string s1;
+	string s2;
+	
+	cin >> s1;
+	cin >> s2;
+	
+	int x = s1.compare(s2);
+	
+	if(x>0)
+		cout << "S1 is larger than S2"<<endl;
+	else if(x<0)
+		cout << "S2 is larger than S1"<<endl;
+	else
+		cout << "S1 and S2 are equal"<<endl;
+		
+	return 0;
+}
